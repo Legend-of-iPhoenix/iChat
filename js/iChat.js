04f0758c9f784d9b974341c72bc54104d01d8048
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-05-07 16:31:06 -0400

    Add assorted changes.
    Adds some extra in-code documentation, some optimizations, and adds new functionality to the plugin API:
    - error throwing when you try to register an invalid plugin
    - `iChat.listPlugins()`, a command whose name is self-documenting

--- js/iChat.js
+++ js/iChat.js
@@ -4,9 +4,6 @@ var iChat = new function () {
   Object.defineProperty(this, 'onload', {
     set(f) {
       iChat.callbacks.push(f);
-    },
-    get() {
-      return iChat.callbacks;
     }
   });
 };
@@ -16,24 +13,32 @@ document.addEventListener("DOMContentLoaded", x => {
     var callbacks = iChat.callbacks;
     iChat = new function () {
       this.version = null;
+      this.releaseDate = null;
       this.isLoaded = false;
       this.plugins = [];
-      this.registerPlugin = function (plugin) {
-        var parser = plugin.parser; 
-        if ((typeof parser == 'function') && parser.length === 1 && !iChat.plugins.find(otherPlugin => otherPlugin.name === plugin.name) && plugin.constructor === iChatPlugin) { // assert that the plugin has a parser that is a function that takes one argument, has a unique name, and was made using the iChatPlugin constructor.
-          iChat.plugins.push(plugin)
-        } // todo: create error message on plugin failure.
-      }
-      this.registerPlugins = function (...plugins) {
-        plugins.forEach(function (plugin) {
-          var parser = plugin.parser;
-          if ((typeof parser == 'function') && parser.length === 1 && !iChat.plugins.find(otherPlugin => otherPlugin.name === plugin.name) && plugin.constructor === iChatPlugin) { // assert that the plugin has a parser that is a function that takes one argument, has a unique name, and was made using the iChatPlugin constructor.
-            iChat.plugins.push(plugin)
-          } // todo: create error message on plugin failure.
+      // Lists plugins. Exists mostly for debugging purposes, but it could be useful somehow.
+      this.listPlugins = function () {
+        console.group("iChat Plugin Listing");
+        iChat.plugins.forEach(function (plugin) {
+          console.log(plugin.name + ": " + plugin.otherInfo);
         });
+        console.groupEnd();
+      }
+      // documented in /docs/plugin-api.md
+      this.registerPlugin = function (plugin) {
+        var parser = plugin.parser;
+        if ((typeof parser == 'function') && parser.length === 1 && !iChat.plugins.find(otherPlugin => otherPlugin.name === plugin.name) && plugin.constructor === iChatPlugin) { // assert that the plugin has a parser function that takes one argument, has a unique name, and was made using the iChatPlugin constructor.
+          iChat.plugins.push(plugin)
+        } else {
+          throw new Error("Invalid iChat plugin!");
+        }
+      }
+      // documented in /docs/plugin-api.md
+      this.registerPlugins = function (...plugins) {
+        plugins.forEach(plugin => iChat.registerPlugin(plugin));
       }
       // removes the plugin with a given name, returning the plugin that was removed.
-      this.removePlugin = function(name) {
+      this.removePlugin = function (name) {
         var removedPlugin = iChat.plugins.find(plugin => plugin.name == name);
         iChat.plugins = iChat.plugins.filter(plugin => plugin.name != name);
         return removedPlugin;
@@ -156,10 +161,12 @@ function iChatPlugin(name, parser, ...otherInfo) {
     data.txt = result;
     return data;
   }, desc);
+  // renders italicized text
   var italics = new iChatPlugin("default/italics", function (data) {
     data.txt = data.txt.replace(/\~([^\~]*)\~/g, '<em style="display: inline-block;">$1</em>');
     return data;
   }, desc);
+  // renders bolded text
   var bold = new iChatPlugin("default/bold", function (data) {
     data.txt = data.txt.replace(/\*([^\~]*)\*/g, '<strong style="display: inline-block;">$1</strong>');
     return data;
